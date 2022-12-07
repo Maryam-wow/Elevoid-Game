3f64b6adb83b6dc9242f028bd227dfc6b3313013
Mariam Hassanein <m.hazem97@gmail.com>
2022-12-07 16:22:28 +0100

    Firetrial

---
+++ Assets/FireAttackScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class FireAttackScript : MonoBehaviour
+{
+    //Vector3 _voidTarget;
+    private int damage = 5;
+    public ParticleSystem particleSystem;
+    //List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+    private void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Fire"))
+        {
+            //LookAtVoid();
+            particleSystem.Play();
+        }
+    }
+    private void OnParticleCollision()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.CompareTag("Void"))
+        {
+                Debug.Log("HIT : " + damage);
+                hit.collider.GetComponentInParent<VoidBehaviour>().TakeDamage(damage);
+        }
+        else
+        {
+            Debug.Log("MISS");
+        }
+        }
+    }
+}
+//void LookAtVoid()
+// {
+//  transform.LookAt(_voidTarget);
+// }
+// }
